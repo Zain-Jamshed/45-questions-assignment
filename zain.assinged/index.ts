c4ad6cdba52d3a51dc4d65b4213badb811bdb30b
Zain-Jamshed <160297829+Zain-Jamshed@users.noreply.github.com>
2024-02-17 22:54:15 -0800

    Add files via upload

---
+++ zain.assinged/index.ts
@@ -0,0 +1,141 @@
+/*Aslam o Alaikum
+My name is Zain, i am 16 years old, i don't have any programming experience but as ever teachers assigned us this 45 questions so i tried my best to learn and solve this questions*/
+
+//Qno2 Personal Message
+let name=" Zain "
+console.log("hey"+name+"lets learn some typsscript today at governor house")
+
+//Qno3 Name Cases
+let case_name = "Zain";
+console.log(case_name.toUpperCase());
+console.log(case_name.toLowerCase());
+
+//Qno4 Famous Quote
+console.log('Allama Iqbal once said,"If one cannot live the life of the brave, then it is better to die like the brave."')
+
+//Qno5 Famous Quote2
+let famous_person="Allama Iqbal once said"
+let message="If one cannot live the life of the brave, then it is better to die like the brave."
+console.log(famous_person+',"'+message+'"')
+
+//Qno6 Stripping Names
+let spaced_name= "\t\n   Zain   \n\t";
+console.log(spaced_name)
+
+//Qno7 Number Eight
+console.log("Addition",4+4)
+console.log("Substraction",10-2)
+console.log("multiplication",2*4)
+console.log("Division",16/2)
+
+//Qno8 
+console.log("Addition",4+4)
+console.log("Substraction",10-2)
+console.log("multiplication",2*4)
+console.log("Division",16/2)
+
+//Qno9 Favorite Number
+let fav_number=( 7 )
+console.log("My favourite number is " +fav_number)
+
+//Qno10 Adding Comments:
+//console.log("Addition",4+4)
+//console.log("Substraction",10-2)
+//console.log("multiplication",2*4)
+//console.log("Division",16/2)
+
+//let spaced_name= "\t\n   Zain   \n\t";
+//console.log(spaced_name)
+
+//Qno11 Names
+let friends_name=["Taha","Ali","Usman","Rasib","Hamza"];
+console.log(friends_name[0]);
+console.log(friends_name[1]);
+console.log(friends_name[2]);
+console.log(friends_name[3]);
+console.log(friends_name[4]);
+
+//Qno12 Greetings
+let friend_msg= " You are blessing to me ";
+console.log(friends_name[0]+friend_msg);
+console.log(friends_name[1]+friend_msg);
+console.log(friends_name[2]+friend_msg);
+console.log(friends_name[3]+friend_msg);
+console.log(friends_name[4]+friend_msg);
+
+//Qno13 Your Own Array
+let my_fav_car=[
+ "My Favourite car is Honda Civic",
+"This car is made by japnese company Honda",
+"This car is so comfortable",
+"The sitting capicity of this car is 4 persons",
+]
+console.log(my_fav_car)
+
+//Qno14 Guest List
+let invited_persons=[" Mamu "," Chachu "," Khala "]
+let guests_message=("I would like to have dinner with you today")
+console.log(guests_message+invited_persons[0])
+console.log(guests_message+invited_persons[1])
+console.log(guests_message+invited_persons[2])
+
+//Qno20 
+let nature=["Mountains","River","Sea","Desserts"]
+console.log(nature)
+
+//Qno24 Conditional Tests
+let x=5;
+let y=9;
+console.log(x>y)
+console.log(x==y)
+console.log(x<y)
+console.log(x!=y)
+let a=8;
+let b=8;
+console.log(a<b)
+console.log(a>b)
+console.log(a==b)
+console.log(a===b)
+console.log(a>x && y<b)
+console.log(a>x || y<b)
+
+
+
+//Qno25 Aliens colors #1
+//let alien_color = "green";
+//if (alien_color === "green") 
+  // console.log('Player just earned 5 points');
+//{
+//let alien_color = "yellow";
+//if (alien_color === "green") 
+  //console.log('Player just earned 5 points');
+//}
+
+
+//Qno26 Aliens colors #1
+let alien_color2= "green"
+let alien_color2= "red"
+{
+if(alien_color2 === "green")
+console.log("You just earned 5 point")
+else (alien_color2 === "red")
+console.log("You just earned 10 point")
+}
+
+//Qno29 Favorite Fruit
+let favourite_fruits= [" Banana "," Mango "," Apple "]
+console.log(favourite_fruits [0]+ "is my favourite Fruit")
+console.log(favourite_fruits [1]+ "is my favourite fruit")
+console.log(favourite_fruits [2]+ "is my favourite fruit")
+
+//Qno30 hello admin
+let persons=[" Admin "," Ali "," Zain "," Rasib "," Ahmed ",]
+
+  console.log("Hello"+ persons[0]+ "would you like to see a status report?")
+  console.log("Hello"+persons[1]+"thank you for logging in again")
+  console.log("Hello"+persons[2]+"thank you for logging in again")
+  console.log("Hello"+persons[3]+"thank you for logging in again")
+  console.log("Hello"+persons[4]+"thank you for logging in again")
+
+
+  
